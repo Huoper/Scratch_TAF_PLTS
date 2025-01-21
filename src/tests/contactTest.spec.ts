dc8e660e041180b07e02f1eec8d5174e62b3a6c0
Huoper <jocker.huop@gmail.com>
2025-01-21 18:31:48 +0200

    add example of using prepared data

--- src/tests/contactTest.spec.ts
+++ src/tests/contactTest.spec.ts
@@ -1,16 +1,20 @@
 import {test} from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 import logger from "../utils/LoggerUtil";
+import users from "../data/userData.json";
 
-test('Contact creation test', async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigateToLoginPage();
-
-    const homePage = await loginPage.login(process.env.userid!, process.env.password!);
-    await homePage.verifyServiceTitle();
-    logger.info('Login test passed');
-    const contactPage = await homePage.navigateToContactPage();
-    await contactPage.createContact('John', 'Doe');
-    await contactPage.verifyContactCreated('John', 'Doe');
-    logger.info('Contact creation test passed');
-});
+for (const user of users) {
+    test(`Contact creation test for ${user.firstName} ${user.lastName}`,
+        { tag: ['@ddt'] }, async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigateToLoginPage();
+    
+        const homePage = await loginPage.login(process.env.userid!, process.env.password!);
+        await homePage.verifyServiceTitle();
+        logger.info('Login test passed');
+        const contactPage = await homePage.navigateToContactPage();
+        await contactPage.createContact(user.firstName!, user.lastName!);
+        await contactPage.verifyContactCreated(user.firstName!, user.lastName!);
+        logger.info('Contact creation test passed');
+    });
+}
